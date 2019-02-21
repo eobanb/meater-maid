8b14d7c5ca4a0a778d4617cb8cfdd1b011aa15e9
Eoban Binder <ebinder@indiana.edu>
2019-02-20 22:09:26 -0500

    Update meater-maid.js

--- meater-maid.js
+++ meater-maid.js
@@ -1,16 +1,18 @@
-module.paths.push('/usr/local/lib/node_modules'); // needed on macOS
+// meter maid 0.1
+
+module.paths.push('/usr/local/lib/node_modules'); // needed on macOS; your node_modules path may differ
 
 const puppeteer = require('puppeteer-core'); // optionally change to 'puppeteer' instead of puppeteer-core
 
 (async () => {
   const browser = await puppeteer.connect({
     browserWSEndpoint: 'ws://127.0.0.1:9222/devtools/browser/eb439886-ad8f-412b-ac26-a4a4495a5ccf'
-  }); // optionally change to .launch (using puppeteer)
+  }); // optionally change to .launch (using puppeteer), have not tested this however
   const page = await browser.newPage();
   await page.goto('https://cooks.cloud.meater.com/cook/9b9b7eb0-bd79-4c29-a8f4-a218b8122371'); // set this to your own URL
   await page.waitForSelector('.internal-value'); // wait for page javascript to finish running
   
-  while(1) { // loop
+  while(1) { // start loop
   	const element = await page.$(".target-value"); // get the element from the DOM
   	const tempvalue = await page.evaluate(element => element.textContent, element); // get the element text
   	console.log(tempvalue); // output temp value to console
